81c3dad469e8b024475e49c40ae9788337ffce7a
Oscar <>
2023-11-01 20:07:02 +0000

    Chnage Namespace

--- PIApp/FileServer.cs
+++ PIApp/FileServer.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PIApp
+namespace PIApp_Lib
 {
     internal class FileServer
     {
--- PIApp/Listener.cs
+++ PIApp/Listener.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PIApp
+namespace PIApp_Lib
 {
     public static class Listener
     {
